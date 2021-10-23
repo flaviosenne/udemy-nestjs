74ae024f8bccc32600e59c02d765506857382fab
joao flavio <flaviosenne123@gmail.com>
2021-10-23 06:06:01 -0300

    create controller

--- api-smartranking/src/app.module.ts
+++ api-smartranking/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { JogadoresModule } from './jogadores/jogadores.module';
 @Module({
-  imports: [],
+  imports: [JogadoresModule],
   controllers: [],
   providers: [],
 })
---
+++ api-smartranking/src/jogadores/jogadores.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Post } from '@nestjs/common';
+
+@Controller('api/v1/jogadores')
+export class JogadoresController {
+
+    @Post()
+    async criarAtualizarJogador() {
+        return JSON.stringify({
+            'name':'joao'
+        })
+    }
+}
