3293308b957574f16e9a1d81c44cc6679f7dd2ee
joao flavio <flaviosenne123@gmail.com>
2021-12-07 08:52:13 -0300

    add config general to environment variable

--- micro-service-api-smartranking/api-gateway/src/app.module.ts
+++ micro-service-api-smartranking/api-gateway/src/app.module.ts
@@ -3,10 +3,14 @@ import { CategoriesModule } from './categories/categories.module';
 import { PlayersModule } from './players/players.module';
 import { ProxyRMQModule } from './proxymq/proxymq.module';
 import { AwsModule } from './aws/aws.module';
+import {ConfigModule} from '@nestjs/config'
 
 
 @Module({
-  imports: [CategoriesModule, PlayersModule, ProxyRMQModule, AwsModule],
+  imports: [
+    CategoriesModule, PlayersModule, 
+    ProxyRMQModule, AwsModule,
+    ConfigModule.forRoot({isGlobal: true})],
   controllers: [],
   providers: [],
 })
--- micro-service-api-smartranking/api-gateway/src/aws/impl/file-aws-impl.ts
+++ micro-service-api-smartranking/api-gateway/src/aws/impl/file-aws-impl.ts
@@ -1,31 +1,40 @@
 import { FileService } from "../file.protocol";
 import * as AWS from 'aws-sdk'
 import { Logger } from "@nestjs/common";
+import {ConfigService} from '@nestjs/config'
 
 export class FileServiceAwsImpl implements FileService {
 
     private logger = new Logger(FileServiceAwsImpl.name)
 
+    constructor(private configService: ConfigService){}
+
     upload(buffer: Buffer, path: string) {
+        const AWS_S3_BUCKET_NAME = this.configService.get<string>('AWS_S3_BUCKET_NAME')
+        const AWS_REGION = this.configService.get<string>('AWS_REGION')
+        const AWS_ACCESS_KEY_ID = this.configService.get<string>('AWS_ACCESS_KEY_ID')
+        const AWS_SECRET_ACCESS_KEY = this.configService.get<string>('AWS_SECRET_ACCESS_KEY')
+        
+        
         const s3 = new AWS.S3({
-            region: '',
-            accessKeyId: '',
-            secretAccessKey: ''
+            region: AWS_REGION,
+            accessKeyId: AWS_ACCESS_KEY_ID,
+            secretAccessKey: AWS_SECRET_ACCESS_KEY
         })
 
         this.logger.log(`path: ${path}`)
 
         const params = {
             Body: buffer,
-            Bucket: '',
+            Bucket: AWS_S3_BUCKET_NAME,
             Key: path
         }
 
         const data = s3
             .putObject(params)
             .promise()
-            .then(data => { return { 
-                url:`https://bucket-name.s3-region.amazonaws.com/${path}` } 
+            .then(_ => { return { 
+                url:`https://${AWS_S3_BUCKET_NAME}.s3-${AWS_REGION}.amazonaws.com/${path}` } 
             },
                 err => {
                     this.logger.error(err)
