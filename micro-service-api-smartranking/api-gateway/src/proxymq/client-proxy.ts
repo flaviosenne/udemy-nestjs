132286e11f2c026ec829f39386d96368cf673f7a
joao flavio <flaviosenne123@gmail.com>
2021-12-07 08:59:41 -0300

    move credentials rabbit mq to environment variable

--- micro-service-api-smartranking/api-gateway/src/proxymq/client-proxy.ts
+++ micro-service-api-smartranking/api-gateway/src/proxymq/client-proxy.ts
@@ -1,13 +1,20 @@
 import { Injectable } from "@nestjs/common";
 import { ClientProxy, ClientProxyFactory, Transport } from "@nestjs/microservices";
+import {ConfigService} from '@nestjs/config'
 
 @Injectable()
 export class ClientProxySmartRanking {
+
+    constructor(private configService: ConfigService){}
+
     getClientProxyAdminBackEndInstance(): ClientProxy {
+        const RABBITMQ_USER = this.configService.get<string>('RABBITMQ_USER')
+        const RABBITMQ_PASSWORD = this.configService.get<string>('RABBITMQ_PASSWORD')
+
         return ClientProxyFactory.create({
             transport: Transport.RMQ,
             options: {
-                urls:['amqps://vcrcblpo:FMqPXeb283pl9oumB-mRu3XKCYv_kl6g@fish.rmq.cloudamqp.com/vcrcblpo'],
+                urls:[`amqps://${RABBITMQ_USER}:${RABBITMQ_PASSWORD}@fish.rmq.cloudamqp.com/vcrcblpo`],
                 queue: 'admin-backend'
             }
         })
